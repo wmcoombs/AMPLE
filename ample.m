219288888eeaaa3546b0c6f5fac7cc0c940fabf7
wmcoombs <w.m.coombs@durham.ac.uk>
2022-02-15 12:04:39 +0000

    main AMPLE 1.1 update
    Update of main file for AMPLE 1.1

--- ample.m
+++ ample.m
@@ -1,7 +1,7 @@
-%AMPLE: A Material Point Learning Environment
+%AMPLE 1.1: A Material Point Learning Environment
 %--------------------------------------------------------------------------
 % Author: William Coombs
-% Date:   29/01/2019
+% Date:   27/08/2020
 % Description:
 % Large deformation elasto-plastic (EP) material point method (MPM) code
 % based on an updated Lagrangian (UL) descripition of motion with a 
@@ -20,12 +20,15 @@
 %--------------------------------------------------------------------------
 clear;
 addpath('constitutive','functions','plotting','setup');        
-[lstps,g,mpData,mesh] = setupGrid_beam;                                          % setup information
+[lstps,g,mpData,mesh] = setupGrid;                                          % setup information
 NRitMax = 10; tol = 1e-9;                                                   % Newton Raphson parameters
 [nodes,nD] = size(mesh.coord);                                              % number of nodes and dimensions
 [nels,nen] = size(mesh.etpl);                                               % number of elements and nodes/element
 nDoF = nodes*nD;                                                            % total number of degrees of freedom
 nmp  = length(mpData);                                                      % number of material points
+lstp = 0;                                                                   % zero loadstep counter (for plotting function)
+uvw  = zeros(nDoF,1);                                                       % zeros displacements (for plotting function)
+run postPro;                                                                % plotting initial state & mesh
 for lstp=1:lstps                                                            % loadstep loop
   fprintf(1,'\n%s %4i %s %4i\n','loadstep ',lstp,' of ',lstps);             % text output to screen (loadstep)
   [mesh,mpData] = elemMPinfo(mesh,mpData);                                  % material point - element information
@@ -48,6 +51,6 @@ for lstp=1:lstps                                                            % lo
     NRit = NRit+1;                                                          % increment the NR counter
     fprintf(1,'%s %2i %s %8.3e\n','  iteration ',NRit,' NR error ',fErr);   % text output to screen (NR error)
   end                           
-  mpData = updateMPs(uvw,mpData);                                           % upate material points
+  mpData = updateMPs(uvw,mpData);                                           % update material points
   run postPro;                                                              % Plotting and post processing 
 end
