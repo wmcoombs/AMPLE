79710c4ad388680cd7db4237440998c763b01dd0
Will Coombs <w.m.coombs@durham.ac.uk>
2025-03-03 13:46:32 +0000

    Add files via upload

--- setup/formCoord2D.m
+++ setup/formCoord2D.m
@@ -1,4 +1,4 @@
-function [etpl,coord] = formCoord2D(nelsx,nelsy,lx,ly)
+function [etpl,coord,ftpl,fntpl,efnum] = formCoord2D(nelsx,nelsy,lx,ly)
 
 %Two dimensional finite element grid generation
 %--------------------------------------------------------------------------
@@ -9,7 +9,7 @@ function [etpl,coord] = formCoord2D(nelsx,nelsy,lx,ly)
 % elements.
 %
 %--------------------------------------------------------------------------
-% [etpl,coord] = FORMCOORD2D(nelsx,nelsy,lx,ly)
+% [etpl,coord,ftpl,fntpl,efnum] = FORMCOORD2D(nelsx,nelsy,lx,ly)
 %--------------------------------------------------------------------------
 % Input(s):
 % nelsx - number of elements in the x direction
@@ -20,6 +20,9 @@ function [etpl,coord] = formCoord2D(nelsx,nelsy,lx,ly)
 % Ouput(s);
 % etpl  - element topology
 % coord - nodal coordinates
+% ftpl  - face-element connections
+% fntpl - face nodal topology
+% efnum - local element face numbers associated with each face
 %--------------------------------------------------------------------------
 % See also:
 %
@@ -50,5 +53,51 @@ for nely=1:nelsy
     etpl(nel,3)=etpl(nel,2)+1;
     etpl(nel,4)=etpl(nel,1)+1; 
   end
+end
+
+if nargout>2
+%% face generation (interior only)
+
+nfaces = (nelsx-1)*nelsy + (nelsy-1)*nelsx;
+ftpl   = zeros(nfaces,2);
+fntpl  = zeros(nfaces,2);
+efnum  = zeros(nfaces,2);
+face   = 0;
+for nely=1:nelsy
+  for nelx=1:nelsx-1
+    face=face+1;
+    e1 = (nely-1)*(nelsx)+nelx;
+    e2 = e1+1;
+    ftpl(face,:) = [e1 e2];
+    
+    n1 = (nely-1)*(nelsx+1)+nelx+1;
+    n2 = (nely)*(nelsx+1)+nelx+1;
+    fntpl(face,:) = [n1 n2];
+    
+    efnum(face,:) = [1 3];
+  end
+end
+
+for nelx=1:nelsx
+  for nely=1:nelsy-1
+    face=face+1;
+    e1 = (nely-1)*(nelsx)+nelx;
+    e2 = e1+nelsx;
+    ftpl(face,:) = [e1 e2];
+    
+    n1 = (nely)*(nelsx+1)+nelx;
+    n2 = n1+1;
+    fntpl(face,:) = [n1 n2];    
+    
+    efnum(face,:) = [2 4];
+  end
+end
+
+% figure;
+% for i = 1:nfaces
+%     plot(coord(fntpl(i,:)',1),coord(fntpl(i,:)',2),'b-'); hold on;
+% end
+% axis equal; axis off;
+
 end
 end
